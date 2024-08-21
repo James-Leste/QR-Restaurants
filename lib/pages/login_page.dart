2b1290e43daeed2ecc1581004f46559707b2304e
Ziqi Wang <wangziqi2385823041@outlook.com>
2024-08-21 18:22:10 +0300

    rearrange the components to make upper-most widgets and bottom-most widgets appear at the same place all the time

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -58,97 +58,115 @@ class _LoginPageState extends State<LoginPage> {
         backgroundColor: Colors.grey[300],
         body: SafeArea(
             child: Center(
-                child: SingleChildScrollView(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              const SizedBox(
-                height: 50,
-              ),
-              const Icon(Icons.lock, size: 100),
-              const SizedBox(height: 50),
-              Text(
-                "Sign in with an existing account",
-                style: TextStyle(color: Colors.grey[700], fontSize: 16),
-              ),
-              const SizedBox(height: 25),
-              MyInputField(
-                controller: emailController,
-                hintText: "Username",
-                obscureText: false,
-              ),
-              const SizedBox(height: 15),
-              MyInputField(
-                controller: passwordController,
-                hintText: "Password",
-                obscureText: true,
-              ),
-              const SizedBox(height: 10),
               Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
+                  padding: const EdgeInsets.symmetric(vertical: 50.0),
+                  child: Column(
+                    children: [
+                      const Icon(Icons.lock, size: 100),
+                      const SizedBox(height: 50),
+                      Text(
+                        "Sign in with an existing account",
+                        style: TextStyle(color: Colors.grey[700], fontSize: 16),
+                      ),
+                    ],
+                  )),
+              // const SizedBox(
+              //   height: 50,
+              // ),
+
+              Expanded(
+                  child: SingleChildScrollView(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Text('Forget password?',
-                        style: TextStyle(color: Colors.grey[700]))
+                    MyInputField(
+                      controller: emailController,
+                      hintText: "Username",
+                      obscureText: false,
+                    ),
+                    const SizedBox(height: 15),
+                    MyInputField(
+                      controller: passwordController,
+                      hintText: "Password",
+                      obscureText: true,
+                    ),
+                    const SizedBox(height: 10),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          Text('Forget password?',
+                              style: TextStyle(color: Colors.grey[700]))
+                        ],
+                      ),
+                    ),
+                    const SizedBox(height: 25),
+                    MyButton(
+                      onTap: signin,
+                      buttonLabel: 'Sign In',
+                    ),
+                    SizedBox(height: 50),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Expanded(
+                            child: Divider(
+                                thickness: 0.3, color: Colors.grey[400])),
+                        Text(
+                          'Or continue with',
+                          style: TextStyle(color: Colors.grey[700]),
+                        ),
+                        Expanded(
+                            child: Divider(
+                                thickness: 0.3, color: Colors.grey[400]))
+                      ],
+                    ),
+                    SizedBox(height: 25),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        SquareTile(
+                          imagePath: 'lib/images/google.png',
+                          onTap: printMessage,
+                        ),
+                        const SizedBox(
+                          width: 25,
+                        ),
+                        SquareTile(
+                          imagePath: 'lib/images/apple-logo.png',
+                          onTap: printMessage,
+                        )
+                      ],
+                    ),
                   ],
                 ),
-              ),
-              const SizedBox(height: 25),
-              MyButton(
-                onTap: signin,
-                buttonLabel: 'Sign In',
-              ),
-              SizedBox(height: 50),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Expanded(
-                      child: Divider(thickness: 0.3, color: Colors.grey[400])),
-                  Text(
-                    'Or continue with',
-                    style: TextStyle(color: Colors.grey[700]),
-                  ),
-                  Expanded(
-                      child: Divider(thickness: 0.3, color: Colors.grey[400]))
-                ],
-              ),
-              SizedBox(height: 25),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  SquareTile(
-                    imagePath: 'lib/images/google.png',
-                    onTap: printMessage,
-                  ),
-                  const SizedBox(
-                    width: 25,
-                  ),
-                  SquareTile(
-                    imagePath: 'lib/images/apple-logo.png',
-                    onTap: printMessage,
-                  )
-                ],
-              ),
-              const SizedBox(height: 100),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    'Not a member?',
-                    style: TextStyle(color: Colors.grey[700]),
-                  ),
-                  const SizedBox(width: 4),
-                  GestureDetector(
-                    onTap: widget.onTap,
-                    child: Text('Register now',
-                        style: TextStyle(
-                            color: Colors.blue, fontWeight: FontWeight.bold)),
-                  ),
-                ],
-              )
+              )),
+
+              Padding(
+                  padding: const EdgeInsets.only(bottom: 50),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Not a member?',
+                        style: TextStyle(color: Colors.grey[700]),
+                      ),
+                      const SizedBox(width: 4),
+                      GestureDetector(
+                        onTap: widget.onTap,
+                        child: Text('Register now',
+                            style: TextStyle(
+                                color: Colors.blue,
+                                fontWeight: FontWeight.bold)),
+                      ),
+                    ],
+                  ))
             ],
           ),
-        ))));
+        )));
   }
 }
--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -63,91 +63,109 @@ class _RegisterPageState extends State<RegisterPage> {
         backgroundColor: Colors.grey[300],
         body: SafeArea(
             child: Center(
-                child: SingleChildScrollView(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              const SizedBox(
-                height: 50,
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 50),
+                child: Column(
+                  children: [
+                    const Icon(Icons.lock, size: 100),
+                    const SizedBox(height: 50),
+                    Text(
+                      "Get a new account",
+                      style: TextStyle(color: Colors.grey[700], fontSize: 16),
+                    ),
+                  ],
+                ),
               ),
-              const Icon(Icons.lock, size: 100),
-              const SizedBox(height: 50),
-              Text(
-                "Register a new account",
-                style: TextStyle(color: Colors.grey[700], fontSize: 16),
-              ),
-              const SizedBox(height: 25),
-              MyInputField(
-                controller: emailController,
-                hintText: "Username",
-                obscureText: false,
-              ),
-              const SizedBox(height: 15),
-              MyInputField(
-                controller: passwordController,
-                hintText: "Password",
-                obscureText: true,
-              ),
-              const SizedBox(height: 10),
-              MyInputField(
-                  controller: confirmPasswordController,
-                  hintText: "Confirm Password",
-                  obscureText: true),
-              const SizedBox(height: 25),
-              MyButton(
-                onTap: signUp,
-                buttonLabel: 'Sign Up',
-              ),
-              const SizedBox(height: 25),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Expanded(
-                      child: Divider(thickness: 0.3, color: Colors.grey[400])),
-                  Text(
-                    'Or continue with',
-                    style: TextStyle(color: Colors.grey[700]),
-                  ),
-                  Expanded(
-                      child: Divider(thickness: 0.3, color: Colors.grey[400]))
-                ],
-              ),
-              SizedBox(height: 25),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  SquareTile(
-                    imagePath: 'lib/images/google.png',
-                    onTap: printMessage,
-                  ),
-                  const SizedBox(
-                    width: 25,
-                  ),
-                  SquareTile(
-                    imagePath: 'lib/images/apple-logo.png',
-                    onTap: printMessage,
-                  )
-                ],
-              ),
-              const SizedBox(height: 50),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    'Not a member?',
-                    style: TextStyle(color: Colors.grey[700]),
-                  ),
-                  const SizedBox(width: 4),
-                  GestureDetector(
-                    onTap: widget.onTap,
-                    child: Text('Login Now',
-                        style: TextStyle(
-                            color: Colors.blue, fontWeight: FontWeight.bold)),
-                  ),
-                ],
+              Expanded(
+                  child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    MyInputField(
+                      controller: emailController,
+                      hintText: "Username",
+                      obscureText: false,
+                    ),
+                    const SizedBox(height: 15),
+                    MyInputField(
+                      controller: passwordController,
+                      hintText: "Password",
+                      obscureText: true,
+                    ),
+                    const SizedBox(height: 15),
+                    MyInputField(
+                        controller: confirmPasswordController,
+                        hintText: "Confirm Password",
+                        obscureText: true),
+                    const SizedBox(height: 25),
+                    MyButton(
+                      onTap: signUp,
+                      buttonLabel: 'Sign Up',
+                    ),
+                    const SizedBox(height: 25),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Expanded(
+                            child: Divider(
+                                thickness: 0.3, color: Colors.grey[400])),
+                        Text(
+                          'Or continue with',
+                          style: TextStyle(color: Colors.grey[700]),
+                        ),
+                        Expanded(
+                            child: Divider(
+                                thickness: 0.3, color: Colors.grey[400]))
+                      ],
+                    ),
+                    SizedBox(height: 25),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        SquareTile(
+                          imagePath: 'lib/images/google.png',
+                          onTap: printMessage,
+                        ),
+                        const SizedBox(
+                          width: 25,
+                        ),
+                        SquareTile(
+                          imagePath: 'lib/images/apple-logo.png',
+                          onTap: printMessage,
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+              )),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 50),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Not a member?',
+                          style: TextStyle(color: Colors.grey[700]),
+                        ),
+                        const SizedBox(width: 4),
+                        GestureDetector(
+                          onTap: widget.onTap,
+                          child: Text('Login Now',
+                              style: TextStyle(
+                                  color: Colors.blue,
+                                  fontWeight: FontWeight.bold)),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
               )
             ],
           ),
-        ))));
+        )));
   }
 }
